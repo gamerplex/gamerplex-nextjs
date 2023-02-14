5a062c8d70efb59033c3b3c614fbcc9c01df36f3
Salim Karim <metasal@gmx.com>
2023-02-14 15:22:09 +1100

    draft

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import localFont from '@next/font/local'
+
+const myFont = localFont({ src: './font.otf' })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <main className={myFont.className}>
+  <Component {...pageProps} />
+  </main>
+  )
 }
